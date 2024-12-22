ca5a69bdd22e0f00151ca36e7a12806a70bd4ba2
Gabriel GRobier <gabriel.grobier@gmail.com>
2024-12-22 00:31:47 -0300

    se realiza prueba con actualizacion de task definition

--- repo/src/app.js
+++ repo/src/app.js
@@ -3,10 +3,12 @@ const axios = require('axios');
 
 const app = express();
 
+// Ruta principal
 app.get('/', (req, res) => {
-    res.send('Ejercicio Modulo 5 desafio 2 por Gabriel Grobier- se hace 4 cambio para subir  ');
+    res.send('Ejercicio Modulo 5 desafio 2 por Gabriel Grobier - se hace 0 cambios para subir');
 });
 
+// Ruta para obtener datos de una API externa
 app.get('/api', async (req, res) => {
     try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
@@ -16,6 +18,12 @@ app.get('/api', async (req, res) => {
     }
 });
 
+// Ruta de health check
+app.get('/health', (req, res) => {
+    res.send('Si ves esto, el servicio está OK');
+});
+
+// Configuración del puerto y arranque del servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);
